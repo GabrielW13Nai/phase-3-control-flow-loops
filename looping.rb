62086d6b1615628b9945e41376ac833f07ebebba
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-03-10 19:34:27 +0300

    Lab done

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
-  # your code here
+  countdown = 10
+  until countdown == 0
+    puts "#{countdown}"
+    countdown= countdown - 1  
+end
+
+puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -15,10 +21,33 @@ def fizzbuzz(num)
   end
 end
 
+
 def fizzbuzz_printer
-  # your code here
+  i = 1
+  until i == 101
+    puts fizzbuzz(i)
+    i=i+1
+  end
 end
 
+
 def reverse_string(str)
-  # your code here
+
+output = ''
+
+length = str.length
+
+i=0
+while i < length
+  output = str[i] + output
+  i+=1
 end
+p output
+end
+
+happy_new_year
+
+fizzbuzz_printer
+
+reverse_string "hello"
+
